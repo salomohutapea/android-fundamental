ae7237a9786e4f173d9b2fc7924d5b956aff85c0
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-16 23:01:16 +0700

    Add viewbinding to HeroAdapter

--- app/src/main/java/com/example/fundamentalandroid/HeroAdapter.kt
+++ app/src/main/java/com/example/fundamentalandroid/HeroAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.fundamentalandroid.databinding.ItemHeroBinding
 import de.hdodenhof.circleimageview.CircleImageView
 
 class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
@@ -28,14 +29,12 @@ class HeroAdapter internal constructor(private val context: Context) : BaseAdapt
         return itemView
     }
 
-    private inner class ViewHolder(view: View) {
-        private val txtName: TextView = view.findViewById(R.id.txt_name)
-        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
-        private val imgPhoto: CircleImageView = view.findViewById(R.id.img_photo)
+    private inner class ViewHolder(private val view: View) {
+        private val binding = ItemHeroBinding.bind(view)
         fun bind(hero: Hero) {
-            txtName.text = hero.name
-            txtDescription.text = hero.description
-            imgPhoto.setImageResource(hero.photo)
+            binding.txtName.text = hero.name
+            binding.txtDescription.text = hero.description
+            binding.imgPhoto.setImageResource(hero.photo)
         }
     }
 }
