72f97dbf5b3ce9c3d94ce0b8767d8b8304ffe8aa
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-16 22:42:42 +0700

    Unit testing

--- app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -1,47 +1,89 @@
 package com.example.fundamentalandroid
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var btnSetValue: Button
-    private lateinit var tvText: TextView
-    private lateinit var imgPreview: ImageView
+    private lateinit var mainViewModel: MainViewModel
 
-    private var names = ArrayList<String>()
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var tvResult: TextView
+    private lateinit var btnCalculateVolume: Button
+    private lateinit var btnCalculateSurfaceArea: Button
+    private lateinit var btnCalculateCircumference: Button
+    private lateinit var btnSave: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSetValue = findViewById(R.id.btn_set_value)
-        tvText = findViewById(R.id.tv_text)
+        mainViewModel = MainViewModel(CuboidModel())
 
-        btnSetValue.setOnClickListener(this)
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        tvResult = findViewById(R.id.tv_result)
+        btnCalculateVolume = findViewById(R.id.btn_calculate_volume)
+        btnCalculateCircumference = findViewById(R.id.btn_calculate_circumference)
+        btnCalculateSurfaceArea = findViewById(R.id.btn_calculate_surface_area)
+        btnSave = findViewById(R.id.btn_save)
 
-        imgPreview = findViewById(R.id.img_preview)
-        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
-
-        names.add("Narenda Wicaksono")
-        names.add("Kevin")
-        names.add("Yoza")
+        btnSave.setOnClickListener(this)
+        btnCalculateSurfaceArea.setOnClickListener(this)
+        btnCalculateCircumference.setOnClickListener(this)
+        btnCalculateVolume.setOnClickListener(this)
     }
 
-    override fun onClick(view: View) {
-        if (view.id == R.id.btn_set_value) {
-            Log.d("MainActivity", names.toString())
-            val name = StringBuilder()
-            for (i in 0..2) {
-                name.append(names[i]).append("\n")
+    override fun onClick(v: View) {
+        val length = edtLength.text.toString().trim()
+        val width = edtWidth.text.toString().trim()
+        val height = edtHeight.text.toString().trim()
+        when {
+            length.isEmpty() -> edtLength.error = "Field ini tidak boleh kosong"
+            width.isEmpty() -> edtWidth.error = "Field ini tidak boleh kosong"
+            height.isEmpty() -> edtHeight.error = "Field ini tidak boleh kosong"
+            else -> {
+                val l = length.toDouble()
+                val w = width.toDouble()
+                val h = height.toDouble()
+                when (v.id) {
+                    R.id.btn_save -> {
+                        mainViewModel.save(l, w, h)
+                        visible()
+                    }
+                    R.id.btn_calculate_circumference -> {
+                        tvResult.text = mainViewModel.getCircumference().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_surface_area -> {
+                        tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_volume -> {
+                        tvResult.text = mainViewModel.getVolume().toString()
+                        gone()
+                    }
+                }
             }
-            tvText.text = name.toString()
         }
+    }
+    private fun visible() {
+        btnCalculateVolume.visibility = View.VISIBLE
+        btnCalculateCircumference.visibility = View.VISIBLE
+        btnCalculateSurfaceArea.visibility = View.VISIBLE
+        btnSave.visibility = View.GONE
+    }
+    private fun gone() {
+        btnCalculateVolume.visibility = View.GONE
+        btnCalculateCircumference.visibility = View.GONE
+        btnCalculateSurfaceArea.visibility = View.GONE
+        btnSave.visibility = View.VISIBLE
     }
 }
