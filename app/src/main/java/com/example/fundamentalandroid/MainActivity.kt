be10892c3badb0b2cdf1f316312049d443f3b511
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-20 12:31:05 +0700

    Custom view

--- app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.fundamentalandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import com.example.fundamentalandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setMyButtonEnable()
+
+        binding.myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+        binding.myButton.setOnClickListener { Toast.makeText(this@MainActivity, binding.myEditText.text, Toast.LENGTH_SHORT).show() }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = binding.myEditText.text
+        binding.myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
