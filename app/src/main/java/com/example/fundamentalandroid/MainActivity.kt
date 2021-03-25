03a63fe9b6259ba375eb30a4c94f68a0a6b77da2
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-25 15:05:35 +0700

    Localization

--- app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.fundamentalandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import com.example.fundamentalandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val pokeCount = 3
+        val hello = resources.getString(R.string.hello_world, "Salomo", pokeCount, "Hutapea")
+
+        binding.tvHello.text = hello
+
+        val songCount = 5
+        val pluralText = resources.getQuantityString(R.plurals.numberOfSongsAvailable, songCount, songCount)
+        binding.tvPlural.text = pluralText
+
+        binding.tvXliff.text = resources.getString(R.string.app_homeurl)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_change_settings) {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
