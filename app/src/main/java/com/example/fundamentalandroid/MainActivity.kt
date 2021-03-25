887050b60ebe2cf0cd82b3aa1478324fbfd4fd5e
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-25 14:27:38 +0700

    Bottom Navigation

--- app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.fundamentalandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fundamentalandroid.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
