d70464a0714527fcbc383704bc628369af2045ec
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-25 17:19:56 +0700

    Asynchronous process - Coroutine

--- app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.example.fundamentalandroid.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -21,23 +26,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnStart.setOnClickListener {
-            executor.execute {
-                try {
-                    //simulate process in background thread
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
-                        handler.post {
-                            //update ui in main thread
-                            if (percentage == 100) {
-                                binding.tvStatus.setText(R.string.task_completed)
-                            } else {
-                                binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                            }
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (percentage == 100) {
+                            binding.tvStatus.setText(R.string.task_completed)
+                        } else {
+                            binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
                 }
             }
         }
