62daade321c6b9f3ad8eb7eb0aa5dd49ab39b4ee
Salomo Hutapea <hutapeasalomo@gmail.com>
2021-03-25 17:19:32 +0700

    Asynchronous process - Executor, handler

--- app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.fundamentalandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.fundamentalandroid.databinding.ActivityMainBinding
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            executor.execute {
+                try {
+                    //simulate process in background thread
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        handler.post {
+                            //update ui in main thread
+                            if (percentage == 100) {
+                                binding.tvStatus.setText(R.string.task_completed)
+                            } else {
+                                binding.tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
